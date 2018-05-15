cf8a7fe8d5ae0a320ae79c0d6506c9ff2be9c280
Raj <38701666+mangaraj44u@users.noreply.github.com>
2018-05-15 15:02:08 +0530

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,44 @@
+ 
+       function green() {
+    
+       document.getElementById("raj").style.color= "green";
+       document.getElementById("raj1").style.color = "green";
+       document.getElementById("raj2").style.color = "green";
+       document.getElementById("raj3").style.color = "green";
+       document.getElementById("raj4").style.color = "green";
+      document.getElementById("raj5").style.color = "green";
+      document.getElementById("raj6").style.color = "green";
+         
+     }
+     function red() {
+       document.getElementById("raj").style.color = "red";
+       document.getElementById("raj1").style.color = "red";
+       document.getElementById("raj2").style.color = "red";
+       document.getElementById("raj3").style.color = "red";
+       document.getElementById("raj4").style.color = "red";
+      document.getElementById("raj5").style.color = "red";
+      document.getElementById("raj6").style.color = "red";
+         
+    }
+     function orange() {
+      document.getElementById("raj").style.color = "orange";
+       document.getElementById("raj1").style.color = "orange";
+       document.getElementById("raj2").style.color = "orange";
+       document.getElementById("raj3").style.color = "orange";
+       document.getElementById("raj4").style.color = "orange";
+      document.getElementById("raj5").style.color = "orange";
+      document.getElementById("raj6").style.color = "orange";
+
+   
+    }
+     function purple() {
+      document.getElementById("raj").style.color = "purple";
+       document.getElementById("raj1").style.color = "purple";
+       document.getElementById("raj2").style.color = "purple";
+       document.getElementById("raj3").style.color = "purple";
+       document.getElementById("raj4").style.color = "purple";
+      document.getElementById("raj5").style.color = "purple";
+      document.getElementById("raj6").style.color = "purple";
+  }
+  
+  
